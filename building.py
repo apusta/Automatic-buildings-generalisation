d5638bcfb482165b4c7869316bf63ecd65f835c7
Agata Kapusta <agata.kohl@gmail.com>
2020-08-12 13:46:30 +0200

    Add tipification and symbolization alghoritms

--- building.py
+++ building.py
@@ -1,5 +1,5 @@
 from shapely.geometry import Polygon, Point, shape
-
+import math
 
 class Building:
 
@@ -7,8 +7,18 @@ class Building:
 
         self.properties = properties
         self.polygon = polygon
+        self.buffer = self.polygon.buffer(0.5)
         self.area = area
         self.isRectangle = self.rectangle_check()
+        self.signatureRotation = self.sygnature_check()
+        self.groupId = None
+
+
+    def __eq__(self, other):
+        return self.properties['OBJECTID'] == other.properties['OBJECTID']
+
+    def __hash__(self):
+        return hash(self.properties['OBJECTID'])
 
     def rectangle_check(self):
         # get minimum bounding box around polygon
@@ -35,3 +45,62 @@ class Building:
             return True
         else:
             return False
+
+    def azimuthCount(self, xpp, ypp, xkk, ykk):
+        dX = xkk - xpp
+        dY = ykk - ypp
+        azimuth = 0
+
+        if dX == 0 and dY == 0:
+            azimuth = 0
+        elif dX == 0 and dY > 0:
+            azimuth = math.pi / 2
+        elif dX == 0 and dY < 0:
+            azimuth = (3 / 2) * math.pi
+        elif dX > 0 and dY == 0:
+            azimuth = 0
+        elif dX < 0 and dY == 0:
+            azimuth = math.pi
+        elif dX != 0 and dY != 0:
+            fi = math.atan(abs(dY / dX))
+            if dX > 0 and dY > 0:
+                azimuth = fi
+            elif dX < 0 < dY:
+                azimuth = math.pi - fi
+            elif dX < 0 and dY < 0:
+                azimuth = math.pi + fi
+            elif dX > 0 > dY:
+                azimuth = 2 * math.pi - fi
+
+        return (azimuth*180/math.pi)
+
+
+    def sygnature_check(self):
+        if self.area > 500:
+            return None
+
+        box = self.polygon.minimum_rotated_rectangle
+        x, y = box.exterior.coords.xy
+
+        # Point in the middle of the edge
+        xkk = ((x[1]-x[0])/2)+x[0]
+        ykk = ((y[1]-y[0])/2)+y[0]
+
+        centroidOfBox = box.centroid
+        xpp = centroidOfBox.x
+        ypp = centroidOfBox.y
+
+        angle = - self.azimuthCount(xpp, ypp, xkk, ykk)
+
+        return angle
+
+    def isConnected(self, building):
+        return self.buffer.intersects(building.buffer)
+
+
+
+
+
+
+
+
